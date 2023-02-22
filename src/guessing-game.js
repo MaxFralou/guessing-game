4dd1d10cccee42f99a66b38e4f40165c89e0aa02
Fralou <fralou@protonmail.com>
2023-02-22 18:56:37 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor(min, max, guessNum) {
+      this.min = min;
+      this.max = max;
+      this.guessNum = guessNum;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.guessNum = Math.ceil((this.min + this.max) / 2);
+      return this.guessNum;
     }
-
+  
     lower() {
-
+      this.max = this.guessNum;
     }
-
+  
     greater() {
-
+      this.min = this.guessNum;
     }
-}
+  }
 
 module.exports = GuessingGame;
